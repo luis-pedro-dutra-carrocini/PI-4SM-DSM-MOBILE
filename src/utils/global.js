5fb44acce2d8f1dde9a9c6c02f591133027a0105
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-10-13 20:09:38 -0300

    Incluindo pastas e arquivos necessários no build

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,2 +1,2 @@
-export const LINKAPI= 'http://192.168.100.249:'; //(Casa) //'http://192.168.253.126:' //(Fatec)
+export const LINKAPI= 'http://192.168.253.126:'; //'http://192.168.100.249:' //(Casa) //'http://192.168.253.126:' //(Fatec) // 'http://192.168.50.126' //(Movel)
 export const PORTAPI= '3001';
