c1aa421969d0516802cf06c27b50c0df0da48dca
luis-pedro-dutra-carrocini <luispedrodutracarrocini58@gmail.com>
2025-10-18 14:45:17 -0300

    Add global.js file

---
+++ src/utils/global.js
@@ -0,0 +1,2 @@
+export const LINKAPI= 'http://192.168.100.249:'; //'http://192.168.50.126:' //(Casa) //'http://192.168.253.126:' //(Fatec) // 'http://192.168.50.126:' //(Movel)
+export const PORTAPI= '3001';
